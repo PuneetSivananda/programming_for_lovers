debf665456d3ffc4ca3f1deb2bdb80d0f7825756
PuneetSivananda <puneet.sivananda@gmail.com>
2022-10-17 15:22:01 +0530

    chore: documenting gcd of a and b

--- numbers/gcd.go
+++ numbers/gcd.go
@@ -1,8 +1,15 @@
 package gcd
 
 /*
-Input: integers x and y
-Output: The greatest common divisor of x and y, denoted as GCD(x,y)
+Input: integers a and b
+Output: The greatest common divisor of a and b, denoted as GCD(a,b)
+
+1, start with gcd at 1
+2, for every integer n between 1 and min(a,b)
+	is n a divisor of a
+	is n a divisor of b
+	if yes for both then gcd to be n
+3, after ranging through all these integers the larget number must be the GCD(a,b)
 
 Min2(a,b)
 	if a> b
