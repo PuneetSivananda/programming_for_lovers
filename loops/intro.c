47f01126fcc3271a96db761f89d1fbd3b3c2be23
PuneetSivananda <puneet.sivananda@gmail.com>
2022-11-04 23:40:37 +0530

    chore: trying out c commands

--- loops/intro.c
+++ loops/intro.c
@@ -1,8 +1,9 @@
-#include<cs50.h>
+#include "cs50.h"
 #include<stdio.h>
 // learning c https://www.youtube.com/watch?v=zYierUhIFNQ
+// to run the program type "make intro"
 int main(void){
-    char c = get_char("Do you agree?");
+    char c = getchar();
     if(c=='y'){
         printf("Agreed.\n");
     }
