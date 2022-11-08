40a12a1d846cf0ffae5df3a9e017c8642b424a4a
Puneet Sivananda <puneet.sivananda@gmail.com>
2022-11-08 15:45:23 +0530

    feat: gcd computation code

---
+++ gcd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/*
+TrivialGCD(a, b)
+
+	d <-1
+	m <-min2(a,b)
+	for every integer p from 1 to m
+		if p is a divisor of a,b
+			d <- p
+	return d
+*/
+
+func main() {
+	fmt.Println("Trivial GCD")
+	x := 378
+	y := 273
+	d := TrivialGCD(x, y)
+	fmt.Println(d)
+}
+
+func TrivialGCD(a, b int) int {
+	d := 1
+	m := Min2(a, b)
+	// if p is a divisor of a
+	// if p is a divisor of b
+	// return Y
+
+	for p := 1; p <= m; p++ {
+		if a%p == 0 {
+			if b%p == 0 {
+				d = p
+			}
+		}
+	}
+	return d
+}
+
+func Min2(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
