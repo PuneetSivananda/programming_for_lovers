a384f1fab767ac150e5d57cca315463bc3a47f89
Puneet Sivananda <puneet.sivananda@gmail.com>
2022-11-17 12:40:40 +0530

    feat: refactored

---
+++ numbers/euclid/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+*
+if a>b then
+
+	GCD(a,b) = GCD(a-b, b)
+
+EuclidGCD(a,b):
+
+	while != b
+		if a> b
+			a<- a-b
+		else
+			b<- b-a
+	return a
+
+Breaking down:
+
+	Any shared divisor of a and b must also be a divisor of a-b
+	Any shared divisor of a - b and b must also be a divisor of a
+*/
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func EuclidGCD(a, b int) int {
+	d := 1
+	m := min(a, b)
+	for p := 1; p <= m; p++ {
+		if p%a == 0 && p%b == 0 {
+			d = p
+		}
+	}
+	return d
+}
+
+func main() {
+	var num1, num2 int
+	fmt.Println("Program starts!")
+	fmt.Scanln(&num1, &num2)
+	fmt.Printf("%d, %d \n", num1, num2)
+	ans := EuclidGCD(num1, num2)
+	fmt.Println("GCD:= ", ans)
+}
