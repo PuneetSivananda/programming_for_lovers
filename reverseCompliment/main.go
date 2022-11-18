6b9c9cdc07968998e35fb6c266735a68c5b6c804
Puneet Sivananda <puneet.sivananda@gmail.com>
2022-11-18 20:23:29 +0530

    feat: implementing reverse compliment using inbuilt functionality

--- reverseCompliment/main.go
+++ reverseCompliment/main.go
@@ -1,18 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func Complement(text string) string {
+func otherComplement(text string) string {
 	return ""
 }
 
-func Reverse(text string) string {
-	return ""
+var complementString = strings.NewReplacer(
+	"T", "A", "A", "T", "G", "C", "C", "G",
+	"t", "a", "a", "t", "g", "c", "c", "g",
+)
+
+func Reverse(text string) (reversed string) {
+	for _, v := range text {
+		reversed = string(v) + reversed
+	}
+	return reversed
 }
 
 func main() {
 	var stringA string
 	fmt.Println("Enter a string to compute reverseCompliment")
 	fmt.Scanln(&stringA)
-	fmt.Println(Complement(Reverse(stringA)))
+	c := complementString.Replace(stringA)
+	fmt.Println(Reverse(c))
 }
