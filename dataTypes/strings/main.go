fbd92e8db9e07beabf0cd07cc2b61f1d2caeb626
Puneet Sivananda <puneet.sivananda@gmail.com>
2022-11-22 19:38:53 +0530

    feat: strings in golang

--- dataTypes/strings/main.go
+++ dataTypes/strings/main.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("String!!.")
 	fmt.Println(string('A'))
 	fmt.Println(string(45))
+	// 45 th symbol from ascii chart
+	mj := strconv.Itoa(45)
+	fmt.Println(mj)
+
+	j, err := strconv.Atoi("-37")
+	if err != nil {
+		panic("Error:" + err.Error())
+	}
+	fmt.Println(j)
+
+	x, err2 := strconv.ParseFloat("3.14", 64)
+	if err2 != nil {
+		panic("Error:" + err.Error())
+	}
+	fmt.Println(x)
+	
+	// String concat
+	s := "Hi"
+	t := " Lovers"
+	fmt.Println(s + t)
 }
