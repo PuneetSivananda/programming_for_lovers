721813a7c43d029420ecfbaed602069fd16c325b
Puneet Sivananda <puneet.sivananda@gmail.com>
2022-11-23 15:12:58 +0530

    intial notes for substrigs

--- dataTypes/kmer/main.go
+++ dataTypes/kmer/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Find Kmers of a string")
+	s := "Hi Lovers!"
+	fmt.Println(s[2:5])
+	fmt.Println(s[4:])
+	fmt.Println(s[:5])
+	// note: sub arrays and sub slices have same notations
+
+	a := make([]int, 6)
+	for i := range a {
+		a[i] = -2*i + 1
+	}
+
+	fmt.Println(a)
+	fmt.Println(a[3:5])
+
+	// given a slice a how can we delete at elem of a given index
+
 }
