87bc87db6d94029b2cd05eaadbb9c3b675457d25
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-29 17:57:10 +0530

    feat: sample code

---
+++ fcc/algorithms/3.findSubString.js
@@ -0,0 +1,9 @@
+// https://www.w3schools.com/jsref/jsref_substr.asp
+
+// we know the substring is should be end, we know length 
+// check the end contains the substring by retrieving the char and checking with substring
+function confirmEnding(str, target) {
+  return str;
+}
+
+console.log(confirmEnding("Bastian", "n"));
