7395ef35e70040be937dd9f901e051ea264f3dc2
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-29 17:09:36 +0530

    feat: added the completed code

---
+++ fcc/algorithms/1.maxChars.js
@@ -0,0 +1,11 @@
+function findLongestWordLength(str) {
+  let MAX = 0;
+  str.split(" ").forEach((item) => {
+    if (item.length > MAX) MAX = item.length;
+  });
+  return MAX;
+}
+
+console.log(
+  findLongestWordLength("The quick brown fox jumped over the lazy dog")
+);
