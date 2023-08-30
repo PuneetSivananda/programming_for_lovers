4ee886834b723a2a44a1be3d784412afd019b337
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-30 18:54:19 +0530

    feat: rudimentary implementation

--- fcc/algorithms/6.finderKeepers.js
+++ fcc/algorithms/6.finderKeepers.js
@@ -1,6 +1,17 @@
 function findElement(arr, func) {
-  let num = 0;
-  return num;
+  let ans;
+  let flag = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (flag) {
+      return ans;
+    }
+    if (func(arr[i])) {
+      flag = 1;
+      ans = true;
+    } else {
+      ans = false;
+    }
+  }
 }
 
 console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
