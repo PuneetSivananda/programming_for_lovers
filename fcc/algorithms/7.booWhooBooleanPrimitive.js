a639170f91c8f821efef8db4ef0947be157b373a
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-31 02:39:37 +0530

    feat: updates

--- fcc/algorithms/7.booWhooBooleanPrimitive.js
+++ fcc/algorithms/7.booWhooBooleanPrimitive.js
@@ -1,5 +1,12 @@
 function booWho(bool) {
-  return bool;
+  return Boolean(bool);
 }
 
 console.log(booWho(null));
+console.log(booWho([1, 2, 3]));
+console.log(booWho([].slice));
+console.log(booWho({ a: 1 }));
+console.log(booWho(1));
+console.log(booWho(NaN));
+console.log(booWho(true));
+console.log(booWho("false"));
