9127874bc7aedddfae8def6f383be0e29674a6d4
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-31 02:27:30 +0530

    feat: added the findKeepers solution

--- fcc/algorithms/6.finderKeepers.js
+++ fcc/algorithms/6.finderKeepers.js
@@ -1,17 +1,15 @@
 function findElement(arr, func) {
-  let ans;
-  let flag = 0;
+  let num = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (flag) {
-      return ans;
-    }
-    if (func(arr[i])) {
-      flag = 1;
-      ans = true;
-    } else {
-      ans = false;
-    }
+    num = arr[i];
+    if (func(num)) return num;
   }
+  return undefined;
 }
 
 console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
+console.log(
+  findElement([1, 3, 5, 8, 9, 10], function (num) {
+    return num % 2 === 0;
+  })
+);
