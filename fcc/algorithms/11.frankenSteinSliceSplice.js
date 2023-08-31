4e010e716804172f71bbe19f72a1d96bfc2fe39d
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-31 23:07:14 +0530

    feat: updated the code to use slice and then splice

--- fcc/algorithms/11.frankenSteinSliceSplice.js
+++ fcc/algorithms/11.frankenSteinSliceSplice.js
@@ -1,5 +1,7 @@
 function frankenSplice(arr1, arr2, n) {
-  return arr2;
+  let newArr = arr2.slice(0);
+  newArr.splice(n, 0, ...arr1);
+  return newArr;
 }
 
 console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
