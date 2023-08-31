53b9a21e5e446657f76ffbb8b9e29082f9522967
PuneetSivananda <puneet.sivananda@gmail.com>
2023-08-31 22:16:50 +0530

    feat: added soln

--- fcc/algorithms/10.titleCaseString.js
+++ fcc/algorithms/10.titleCaseString.js
@@ -1,5 +1,9 @@
 function titleCase(str) {
-  return str;
+  const ans = str.split(" ").map((element) => {
+    const smaller = element.toLowerCase();
+    return smaller[0].toUpperCase() + smaller.substring(1);
+  });
+  return ans.join(" ");
 }
 
 console.log(titleCase("I'm a little tea pot"));
