e043bf670b5f2773ca05122903c7a83141ba5d5d
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-02 00:18:46 +0530

    wip: the implementation for where do i belong

--- fcc/algorithms/13.whereDoIBelong.js
+++ fcc/algorithms/13.whereDoIBelong.js
@@ -1,5 +1,15 @@
 function getIndexToIns(arr, num) {
-  return num;
+  let ix;
+  if (num <= arr[0]) return 0;
+  if (num >= arr[arr.length - 1]) return arr.length;
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] < num && num < arr[i + 1]) {
+      ix = i + 1;
+    }
+  }
+  return ix;
 }
 
 console.log(getIndexToIns([40, 60], 50));
+console.log(getIndexToIns([3, 10, 5], 3));
+console.log(getIndexToIns([2, 5, 10], 15));
