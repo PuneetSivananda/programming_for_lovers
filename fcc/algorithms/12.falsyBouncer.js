0b810dfdb08ea8b4e8d3db491e57e9729fd4bc18
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-01 23:42:49 +0530

    feat: completed

--- fcc/algorithms/12.falsyBouncer.js
+++ fcc/algorithms/12.falsyBouncer.js
@@ -1,5 +1,9 @@
 function bouncer(arr) {
-  return arr;
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Boolean(arr[i])) newArr.push(arr[i]);
+  }
+  return newArr;
 }
 
 console.log(bouncer([7, "ate", "", false, 9]));
