727d641ea34946a4215db31abd5a026889203916
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-04 12:55:02 +0530

    chore: completed

--- fcc/algorithms/13.whereDoIBelong.js
+++ fcc/algorithms/13.whereDoIBelong.js
@@ -10,6 +10,6 @@ function getIndexToIns(arr, num) {
   return newArr.length;
 }
 
-// console.log(getIndexToIns([40, 60], 50));
+console.log(getIndexToIns([40, 60], 50));
 console.log(getIndexToIns([5, 3, 20, 3], 5));
-// console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
