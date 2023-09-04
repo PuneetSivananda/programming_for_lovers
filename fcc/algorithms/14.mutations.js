4fd70a0763cfcfea6722d7619ba1884f04603512
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-04 23:29:10 +0530

    feat: this also works

--- fcc/algorithms/14.mutations.js
+++ fcc/algorithms/14.mutations.js
@@ -1,10 +1,16 @@
 function mutation(arr) {
-  const a1 = Array.from(new Set(arr[0].toLowerCase()));
-  const a2 = Array.from(new Set(arr[1].toLowerCase()));
-  for (let i = 0; i < a1.length; i++) {
-    if (a2.indexOf(a1[i] < 0)) return false;
-  }
-  return true;
+  //   const a1 = Array.from(new Set(arr[0].toLowerCase()));
+  //   const a2 = Array.from(new Set(arr[1].toLowerCase()));
+  //   for (let i = 0; i < a1.length; i++) {
+  //     if (a2.indexOf(a1[i] < 0)) return false;
+  //   }
+  //   return true;
+  return arr[1]
+    .toLowerCase()
+    .split("")
+    .every(function (letter) {
+      return arr[0].toLowerCase().indexOf(letter) !== -1;
+    });
 }
 
 console.log(mutation(["hello", "Hello"]));
