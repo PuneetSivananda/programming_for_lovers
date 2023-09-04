4e7f1a50542574e9279e81ea735bb8da8b24ccb4
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-04 23:14:25 +0530

    feat: implementation

--- fcc/algorithms/14.mutations.js
+++ fcc/algorithms/14.mutations.js
@@ -1,5 +1,16 @@
 function mutation(arr) {
-  return arr;
+  const a1 = Array.from(new Set(arr[0]));
+  const a2 = Array.from(new Set(arr[1]));
+  let flag = false;
+  a2.every((item) => {
+    if (a1.includes(item)) flag = true;
+    else {
+      flag = false;
+      return false;
+    }
+  });
+  return flag;
 }
 
-console.log(mutation(["hello", "hey"]));
+console.log(mutation(["hello", "neo"]));
+console.log(mutation(["floor", "for"]));
