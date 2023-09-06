ec56e1bec1514c3499a9d566cbeacee20bc14a86
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-06 17:56:04 +0530

    feat: initial code, thegenerated numbers are not random

--- sumTwoDice/main.go
+++ sumTwoDice/main.go
@@ -1,7 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
 /*
 SumTwoDice()
-	the output cannot be more than 12, 
-	
+	the output cannot be more than 12,
+
 	output 2 nums between 1 to 6 get the sum of these numbers
 */
+
+func main() {
+	fmt.Println("Craps!")
+
+	
+	for i := 0; i < 7; i++ {
+		fmt.Println(RollDie())
+	}
+}
+
+func RollDie() int {
+	return rand.Intn(6) + 1
+}
+func SumTwoDice() int {
+	return RollDie() + RollDie()
+}
