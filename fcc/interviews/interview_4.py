a31693deb99e5d26132c34020e24ef27015af057
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-11 16:08:37 +0530

    feat:informal submission

---
+++ fcc/interviews/interview_4.py
@@ -0,0 +1,40 @@
+# get prime num bewrween 1 to 100
+NUM = 20
+
+
+def check_if_prime(n):
+    is_prime = False
+    for i in range(n):
+        if (i == 0):
+            continue
+        if (n % i == 0):
+            is_prime = False
+            break
+        else:
+            is_prime = True
+    return is_prime
+
+
+def prime_nos():
+    for i in range(1, NUM):
+        if (check_if_prime(i)):
+            print(i)
+        else:
+            continue
+
+
+def fibonacci():
+    a, b = 0, 1
+    counts = []
+    counts.append(1)
+    for i in range(1, 10):
+        a, b = b, a+b
+        counts.append(b)
+
+    for i in range(len(counts)):
+        print(counts[i])
+
+
+fibonacci()
+
+# prime_nos()
