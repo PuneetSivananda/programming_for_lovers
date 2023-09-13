24ebdc2c6db044c6242f92f84667d7988ff5a641
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-13 13:03:23 +0530

    feat: stuck

---
+++ fcc/interviews/interview_5.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Item struct {
+	name string
+	age  int
+}
+
+func main() {
+	var list_of_characters = []struct {
+		name string json:"name"
+		age  int    json:"age"
+	}{
+		{"name": "Ram", "age": 20},
+		{"name": "Sam", "age": 25},
+		{"name": "Sita", "age": 27},
+		{"name": "Gita", "age": 22},
+	}
+
+	for i := 0; i <= len(list_of_characters); i++ {
+		fmt.Println(list_of_characters[i].age)
+	}
+}
