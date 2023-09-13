b3d926ea0a6d48a37afc932e906191afb7a4390f
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-13 16:17:39 +0530

    todo: complete the code

--- fcc/interviews/interview_5.go
+++ fcc/interviews/interview_5.go
@@ -1,3 +1,5 @@
+// https://www.freecodecamp.org/news/iteration-in-golang/
+// https://blog.boot.dev/golang/anonymous-structs-golang/
 package main
 
 import "fmt"
@@ -11,20 +13,22 @@ func main() {
 	SolveUsingMap()
 }
 
-func SolveUsingStruct(){}
+func SolveUsingStruct() {}
 
-func SolveUsingMap(){
-	var list_of_characters = []Item {
-		name string json:"name"
-		age  int    json:"age"
-	}{
+func SolveUsingMap() {
+	list_of_characters := []map[string]interface{}{
 		{"name": "Ram", "age": 20},
 		{"name": "Sam", "age": 25},
 		{"name": "Sita", "age": 27},
 		{"name": "Gita", "age": 22},
 	}
 
-	for i := 0; i <= len(list_of_characters); i++ {
-		fmt.Println(list_of_characters[i].age)
+	for i := 0; i < len(list_of_characters); i++ {
+		for j := 0; j < len(list_of_characters); j++ {
+			if list_of_characters[i]["age"].(int) < list_of_characters[j]["age"].(int) { // Type Assertions (.int)
+				fmt.Println(list_of_characters[i])
+				break
+			}
+		}
 	}
 }
---
+++ fcc/interviews/interview_6.py
@@ -0,0 +1,8 @@
+def get_anagrams(list_of_words):
+    result = {}
+    for word in list_of_words:
+        print(set(word))
+    return
+
+
+get_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
