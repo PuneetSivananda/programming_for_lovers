b8244d860acc306e2a5729c78112e9626c022feb
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-14 14:14:51 +0530

    feaT: completed first question

---
+++ fcc/interviews/interview_7.py
@@ -0,0 +1,54 @@
+valuesA = {'Ram': 'Pen', 'Shyam': 'copy',
+           'Karim': 'Tab', 'Abhishek': 'Pen', 'Ajai': 'copy'}
+
+
+def create_hashmap_values(values):
+    hashmap = {}
+
+    for k, v in valuesA.items():
+        if v not in hashmap.keys():
+            hashmap[v] = [k]
+        else:
+            hashmap[v].append(k)
+    return hashmap
+
+
+print(create_hashmap_values(valuesA))
+
+# hashmap = {}
+# for item in valuesA:
+#     chck if (item[vakue] present in haspmap)
+#     if no create a list with key as value, and append the value to its list
+#     next item check if exists append or create new
+
+
+# {"Pen": []}
+
+
+"""
+def get_if_crcular(item, substr):
+    world = item + item
+    while len(world) > 0:
+        check = world[0:len(substr)]
+        if (check - - substr):
+
+    return item
+
+
+input_1 = input()
+substr = input()
+print(get_if_crcular(input_1, substr))
+
+lista = [[10, 20, 30], [20, 30, 40], [34, 45, 67]]
+
+
+def recurs_items(lista):
+    for item in lista:
+        if (type(item) == 'list'):
+            recurs_items(item)
+        else:
+            print(item)
+
+
+print(recurs_items(lista))
+"""
