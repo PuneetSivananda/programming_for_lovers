1ea021ad9bc8cb9899326bce3b558db4b7de8443
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-15 00:16:03 +0530

    feat: implemented the same in go

--- fcc/algorithms/18.groupedAnagrams/golang.go
+++ fcc/algorithms/18.groupedAnagrams/golang.go
@@ -9,17 +9,24 @@ import (
 // output: [['tan', 'nat'], ['bat'], ['eat', 'tea', 'ate']]
 
 func SortString(input string) string {
-	inputRunes := []rune(input)
-	sort.Slice(inputRunes, func(i, j int) bool {
-		return inputRunes[i] < inputRunes[j]
+	inputRunes := []rune(input)                  //convert string to rune array
+	sort.Slice(inputRunes, func(i, j int) bool { // use the Slice option that takes the array of runes
+		return inputRunes[i] < inputRunes[j] // and sorts the items in the array
 	})
 	return string(inputRunes)
 }
 
 func GetAnagrams(words []string) [][]string {
+	result := map[string][]string{}
 	for i := 0; i < len(words); i++ {
-		fmt.Println(SortString(words[i]))
+		if _, ok := result[SortString(words[i])]; ok {
+
+			result[SortString(words[i])] = append(result[SortString(words[i])], words[i])
+		} else {
+			result[SortString(words[i])] = []string{words[i]}
+		}
 	}
+	fmt.Println(result)
 	return [][]string{
 		{"one"},
 		{"two", "three"},
