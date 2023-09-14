b58def6124278f348fc21b4bba0b35e059266913
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-14 18:15:47 +0530

    fix: return type

--- fcc/algorithms/18.groupedAnagrams/golang.go
+++ fcc/algorithms/18.groupedAnagrams/golang.go
@@ -4,8 +4,16 @@ import "fmt"
 
 // input:  ["eat", "tea", "tan", "ate", "nat", "bat"]
 // output: [['tan', 'nat'], ['bat'], ['eat', 'tea', 'ate']]
-func main() {
-	fmt.Println("Hello world")
+
+func GetAnagrams(words []string) [][]string {
+	return [][]string{
+		{"one"},
+		{"two", "three"},
+	}
 }
 
-
+func main() {
+	input := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+	output := GetAnagrams(input)
+	fmt.Println(output)
+}
