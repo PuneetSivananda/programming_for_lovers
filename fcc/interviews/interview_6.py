0359788e2f89381617d959864578cdbd72ef9b4b
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-14 14:45:11 +0530

    feat: some sort of solution

--- fcc/interviews/interview_6.py
+++ fcc/interviews/interview_6.py
@@ -1,8 +1,19 @@
+# group anagrams
+# get the sorted word for each item in the list
+# compare the sorted word with the dict, if present add, else create new key and add
+
 def get_anagrams(list_of_words):
     result = {}
     for word in list_of_words:
-        print(set(word))
-    return
+        x = "".join(sorted(word))
+        if x in result:
+            result[x].append(word)
+        else:
+            result[x] = [word]
+    for k, v in result.items():
+        print(k, v)
+
+    return result
 
 
-get_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+print(get_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
