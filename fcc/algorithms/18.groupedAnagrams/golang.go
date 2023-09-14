81b4265c71cbf3814ff1504913760533e989f7ea
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-15 00:04:38 +0530

    feat: implement sorting a string in golang

--- fcc/algorithms/18.groupedAnagrams/golang.go
+++ fcc/algorithms/18.groupedAnagrams/golang.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // input:  ["eat", "tea", "tan", "ate", "nat", "bat"]
 // output: [['tan', 'nat'], ['bat'], ['eat', 'tea', 'ate']]
 
+func SortString(input string) string {
+	inputRunes := []rune(input)
+	sort.Slice(inputRunes, func(i, j int) bool {
+		return inputRunes[i] < inputRunes[j]
+	})
+	return string(inputRunes)
+}
+
 func GetAnagrams(words []string) [][]string {
+	for i := 0; i < len(words); i++ {
+		fmt.Println(SortString(words[i]))
+	}
 	return [][]string{
 		{"one"},
 		{"two", "three"},
