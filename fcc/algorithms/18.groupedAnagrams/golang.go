85655eacbfd9a3ce5b401454d7ad5af6f9452f4e
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-14 14:51:49 +0530

    feat: reimplement in golang

---
+++ fcc/algorithms/18.groupedAnagrams/golang.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// input:  ["eat", "tea", "tan", "ate", "nat", "bat"]
+// output: [['tan', 'nat'], ['bat'], ['eat', 'tea', 'ate']]
+func main() {
+	fmt.Println("Hello world")
+}
+
+
