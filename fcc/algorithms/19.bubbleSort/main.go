236bdb1b6651237988eb968dda790f4bc3bc21a5
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-17 22:04:31 +0530

    feat: how to swap items in go

--- fcc/algorithms/19.bubbleSort/main.go
+++ fcc/algorithms/19.bubbleSort/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+func BubbleSort(inputArray []int) []int {
+	fmt.Println(inputArray[1], inputArray[2])
+	fmt.Println(inputArray)
+	inputArray[1], inputArray[2] = inputArray[2], inputArray[1]
+	fmt.Println(inputArray)
+	return []int{1, 23, 5}
+}
+
 func main() {
-	bubbleSortArray := [...]int{6, 7, 3, 2, 1, 7, 8, 9}
+	bubbleSortArray := []int{6, 7, 3, 2, 1, 7, 8, 9}
+	BubbleSort(bubbleSortArray)
 }
