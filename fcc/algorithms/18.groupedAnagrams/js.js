e00324fb9c552ac85652822123c4a7f2a9649a01
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-18 17:04:23 +0530

    feat: implement in js

--- fcc/algorithms/18.groupedAnagrams/js.js
+++ fcc/algorithms/18.groupedAnagrams/js.js
@@ -1,12 +1,14 @@
-function get_anagrams(list_of_words) {}
+function get_anagrams(list_of_words) {
+  result = {};
+  list_of_words.forEach((element) => {
+    const x = element.split("").sort().join("");
+    if (result[x] == undefined) {
+      result[x] = [element];
+    } else {
+      result[x].push(element);
+    }
+  });
+  return result;
+}
 
-console.log(
-  get_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])([
-    "eat",
-    "tea",
-    "tan",
-    "ate",
-    "nat",
-    "bat",
-  ])
-);
+console.log(get_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
