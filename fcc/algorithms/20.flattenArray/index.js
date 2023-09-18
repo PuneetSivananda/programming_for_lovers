e532129f9097c75d7071c7e1434e46d47cccc0e0
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-18 18:07:34 +0530

    feat: sorting the array

--- fcc/algorithms/20.flattenArray/index.js
+++ fcc/algorithms/20.flattenArray/index.js
@@ -52,9 +52,15 @@ for (obj in arr) {
     });
   }
 }
-console.log(output);
+var args = process.argv;
+console.log(
+  "Pass asc as the third arg for getting the list sorted in ascending order"
+);
 const sortedOutput = output.sort((item1, item2) => {
-  return item1.age - item2.age;
+  if (args[2] == "asc") {
+    return item1.age - item2.age;
+  }
+  return item2.age - item1.age;
 });
 
 console.log(sortedOutput);
