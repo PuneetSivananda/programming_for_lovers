442cce64a916603f7b6cd64ff96cb49c6b48453d
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-20 23:23:29 +0530

    feat: represent data in go

--- fcc/algorithms/20.flattenArray/main.go
+++ fcc/algorithms/20.flattenArray/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// flatten the array and sort desc by age
+// ['A', 'B', 'F', 'C', 'D', 'E']
+type Item struct {
+	Name  string
+	Age   int
+	Child []Item
+}
 
 func main() {
 	fmt.Println("flatten an array in golang")
+	arr := []Item{
+		{Name: "A", Age: 60, Child: []Item{
+			{Name: "B", Age: 40},
+			{Name: "C", Age: 30, Child: []Item{
+				{Name: "D", Age: 14},
+				{Name: "E", Age: 5},
+			}},
+		}},
+		{Name: "F", Age: 35},
+	}
+	fmt.Println(arr)
 }
