5cf8a0c0c787e936a5bbc65b9b722537b57c0f9a
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-21 22:53:30 +0530

    feat: bubble sort implementation

---
+++ fcc/algorithms/19.bubbleSort/index.js
@@ -0,0 +1,19 @@
+function bubbleSortAnArray(arr) {
+  let swap = false;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap = true;
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    if (!swap) {
+      return;
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSortAnArray([6, 7, 3, 2, 1, 7, 8, 9]));
