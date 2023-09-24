8cee7c752605d5f30e71f74d1d305cbe73feaa49
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-24 21:09:56 +0530

    feat: pascal pyramid implement

--- fcc/algorithms/21.PyramidsForDummies/pascalPyramid.py
+++ fcc/algorithms/21.PyramidsForDummies/pascalPyramid.py
@@ -1,11 +1,19 @@
+from __future__ import print_function
 rows = 6
+value = 1
 
 
 def printPascalsPyramid(rows):
-    for i in range(rows):
-        for j in range(1+i):
-            print(j+1, end=" ")
-        print("\n")
+    for i in range(rows+1):
+        for _ in range(1, rows - i + 1):
+            print(" ", end="")
+        for j in range(0, i):
+            if j == 0 or i == 0:
+                value = 1
+            else:
+                value *= (i-j)//j
+            print(value, end="  ")
+        print()
     return
 
 
