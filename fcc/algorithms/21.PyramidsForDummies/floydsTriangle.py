0a16adc1de940f1c4cd7c79926f73a3f570d3a54
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-24 21:45:22 +0530

    feat: implementation of floyds triangle

--- fcc/algorithms/21.PyramidsForDummies/floydsTriangle.py
+++ fcc/algorithms/21.PyramidsForDummies/floydsTriangle.py
@@ -2,7 +2,12 @@ rows = 5
 
 
 def FloysTriangle(rows):
-    
+    num = 1
+    for i in range(1, rows+1):
+        for j in range(1, i+1):
+            print(num, end=" ")
+            num += 1
+        print()
     return
 
 
