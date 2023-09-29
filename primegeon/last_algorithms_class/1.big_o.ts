8af14ac297c58f847f7d613b28fb00ee679301e3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-30 00:05:05 +0530

    feat: big 0 computation

---
+++ primegeon/last_algorithms_class/1.big_o.ts
@@ -0,0 +1,9 @@
+function sum_char_codes(n: string): number {
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum += n.charCodeAt(i);
+  }
+  return sum;
+}
+
+console.log(sum_char_codes("sample"));
