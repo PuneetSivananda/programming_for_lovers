a5eb4fcd78b997861fc9145e4e7c87400358e623
PuneetSivananda <puneet.sivananda@gmail.com>
2023-09-30 00:11:43 +0530

    feat: worst case o(n)

--- primegeon/last_algorithms_class/1.big_o.ts
+++ primegeon/last_algorithms_class/1.big_o.ts
@@ -1,10 +1,11 @@
 function sum_char_codes(n: string): number {
   let sum = 0;
   for (let i = 0; i < n.length; i++) {
-    sum += n.charCodeAt(i);
-  }
-  for (let i = 0; i < n.length; i++) {
-    sum += n.charCodeAt(i);
+    const charCode = n.charCodeAt(i);
+    if (charCode === 69) {
+      return sum;
+    }
+    sum += charCode;
   }
   return sum;
 }
