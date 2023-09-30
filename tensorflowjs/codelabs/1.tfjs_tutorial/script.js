47ecfeec3abcc164e3e63b2692a97e943a9f1d4c
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-01 00:20:09 +0530

    feat: get data function

--- tensorflowjs/codelabs/1.tfjs_tutorial/script.js
+++ tensorflowjs/codelabs/1.tfjs_tutorial/script.js
@@ -1,1 +1,16 @@
 console.log("Hello from tensorflow");
+
+async function getData() {
+  const carsDataResponse = await fetch(
+    "https://storage.googleapis.com/tfjs-tutorials/carsData.json"
+  );
+  const carsData = carsDataResponse.json();
+  const cleaned = carsData
+    .map((car) => ({
+      mpg: car.Miles_per_Gallon,
+      horsepower: car.Horsepower,
+    }))
+    .filter((car) => car.mpg != null && car.horsepower != null);
+  return cleaned;
+}
+
