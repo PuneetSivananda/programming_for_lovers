e15e3ca8d8d1fa1648a7c43b003e495b609ef500
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-01 23:45:20 +0530

    feat: visualize the data in tfjs

--- primegeon/last_algorithms_class/2.arrays.ts
+++ primegeon/last_algorithms_class/2.arrays.ts
@@ -1,2 +1,7 @@
 const a = [];
 console.log(a);
+/**
+ * Array = Contigous memory space
+ * we can walk that space we start from the 0th mem address
+ * for each iteration offset by the mem space and add one byte address
+ */
--- tensorflowjs/codelabs/1.tfjs_tutorial/script.js
+++ tensorflowjs/codelabs/1.tfjs_tutorial/script.js
@@ -3,14 +3,33 @@ console.log("Hello from tensorflow");
 async function getData() {
   const carsDataResponse = await fetch(
     "https://storage.googleapis.com/tfjs-tutorials/carsData.json"
-  );
-  const carsData = carsDataResponse.json();
-  const cleaned = carsData
+  ).then((data) => data.json());
+
+  const cleaned = carsDataResponse
     .map((car) => ({
       mpg: car.Miles_per_Gallon,
       horsepower: car.Horsepower,
     }))
     .filter((car) => car.mpg != null && car.horsepower != null);
+
   return cleaned;
 }
 
+async function run() {
+  const data = await getData();
+  const values = data.map((d) => ({
+    x: d.horsepower,
+    y: d.mpg,
+  }));
+  tfvis.render.scatterplot(
+    { name: "Horsepower v MPG" },
+    { values },
+    {
+      xLabel: "Horsepower",
+      yLabel: "MPG",
+      height: 300,
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", run);
