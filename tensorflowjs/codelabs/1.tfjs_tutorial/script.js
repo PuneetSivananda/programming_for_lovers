366123cff291cf0bdd53c557dd4102788090fd08
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-02 15:08:37 +0530

    feat: prepare the data for tranining

--- tensorflowjs/codelabs/1.tfjs_tutorial/script.js
+++ tensorflowjs/codelabs/1.tfjs_tutorial/script.js
@@ -40,8 +40,43 @@ const createModel = () => {
  * Convert the input data to tensors for machine learning
  * implement practices like __shuffling__ and __normalizing__ the data.
  */
+function convertToTensor(data) {
+  return tf.tidy(() => {
+    // 1.Shuffle the data
+    tf.utl.shuffle(data);
 
+    // 2. Convert data to tensor
+    const inputs = data.map((d) => d.horsepower);
+    const labels = data.map((d) => d.mpg);
 
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+    // 3. Normalize the data to the range 0-1 using min-max scaling
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor
+      .sub(inputMin)
+      .div(inputMax)
+      .sub(inputMin);
+    const normalizedLabels = labelTensor
+      .sub(labelMin)
+      .div(labelMax)
+      .sub(labelMin);
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // return the min/max bounds that can be re-used
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    };
+  });
+}
 
 async function run() {
   // get data
