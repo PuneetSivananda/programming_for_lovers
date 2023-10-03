3ac47185d0f059e36934dc7a42b039c2a914f680
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-03 23:58:37 +0530

    wip: stack implementaion

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/Stack.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/Stack.ts
@@ -1,18 +1,18 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private nums: T[];
 
     constructor() {
+        this.nums = [];
     }
 
     push(item: T): void {
-
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+        if (this.nums.length === this.length) {
+            throw Error("The Array is full cannot push");
+        }
+        this.length = this.length + 1;
+        this.nums.push(item);
+    }
+    pop(): T | undefined {}
+    peek(): T | undefined {}
 }
