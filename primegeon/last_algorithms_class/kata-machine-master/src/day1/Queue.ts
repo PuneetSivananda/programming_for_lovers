fdfe2c6f088c3ad7f4f28771602be53326de7255
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-07 23:51:52 +0530

    feat: added the constructor for queue

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/Queue.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/Queue.ts
@@ -1,18 +1,20 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
-    enqueue(item: T): void {
-
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    enqueue(item: T): void {}
+    deque(): T | undefined {}
+    peek(): T | undefined {}
 }
