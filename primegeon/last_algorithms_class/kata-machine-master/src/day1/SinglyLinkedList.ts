5bacf7f5dfa926f5b947d2dcf33df7b8a7ea0ec2
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-07 23:41:17 +0530

    feat: start implementing linked list

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/SinglyLinkedList.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/SinglyLinkedList.ts
@@ -1,27 +1,20 @@
+class Node {
+    public next: Node | null;
+    public value: number;
+    constructor(value: number) {
+        this.next = null;
+        this.value = value;
+    }
+}
 export default class SinglyLinkedList<T> {
     public length: number;
+    public items: Node[];
+    constructor() {}
 
-    
-
-    constructor() {
-    }
-
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
