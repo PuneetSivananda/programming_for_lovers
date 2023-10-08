0a6132689461cd0c09b5f17963d448196fc110be
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-08 09:59:17 +0530

    feat: checks for queue

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/Queue.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/Queue.ts
@@ -14,18 +14,36 @@ export default class Queue<T> {
         this.length = 0;
     }
 
-    enqueue(item: T): void {}
+    enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node; // make new node the tail
+    }
+
     deque(): T | undefined {
         if (!this.head) {
             return undefined;
         }
         this.length--;
+
         const head = this.head;
         this.head = this.head.next;
+
+        //free add gc here
         head.next = undefined;
-        // add gc here
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
         return head.value;
     }
+
     peek(): T | undefined {
         return this.head?.value;
     }
