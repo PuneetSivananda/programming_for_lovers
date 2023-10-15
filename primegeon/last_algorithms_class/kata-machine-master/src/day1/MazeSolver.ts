5bac6fac2ce57cc75451e64a0ea2179a45a05653
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-15 19:08:47 +0530

    feat: added the base case check

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/MazeSolver.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/MazeSolver.ts
@@ -1,4 +1,10 @@
-function walk(maze: string[], wall: string, curr: Point, end: Point): boolean {
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolen[][],
+): boolean {
     // 1 Base case
     // off the map
     if (
@@ -10,7 +16,23 @@ function walk(maze: string[], wall: string, curr: Point, end: Point): boolean {
         return false;
     }
     // 2 On a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+    // 3 are we at the end of the walk
+    if (curr.x === end.x && curr.y == end.y) {
+        seen[curr.y][curr.x] = true;
+        return true;
+    }
+    // 4 if we have seen this point
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+
+    // Recursive Case
+    // pre, recurse, post
     
+
 }
 
 export default function solve(
