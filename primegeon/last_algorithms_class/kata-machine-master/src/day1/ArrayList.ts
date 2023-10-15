05de22b5d5ac2df4d7e97896b9644da5b31cd1e1
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-15 23:34:44 +0530

    chore: test the input and outputs

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/ArrayList.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/ArrayList.ts
@@ -12,7 +12,13 @@ export default class ArrayList<T> {
             return;
         }
     }
-    insertAt(item: T, idx: number): void {}
+    insertAt(item: T, idx: number): void {
+        if (this.length == this.nums.length) {
+            return;
+        }
+        this.nums.splice(idx, 0, item);
+        this.length++;
+    }
     append(item: T): void {
         if (this.length < this.nums.length) {
             this.nums.push(item);
@@ -21,6 +27,7 @@ export default class ArrayList<T> {
     }
     remove(item: T): T | undefined {
         if (this.nums.includes(item)) {
+            this.length--;
             return this.nums.splice(this.nums.indexOf(item), 1)[0];
         } else {
             return undefined;
@@ -28,11 +35,40 @@ export default class ArrayList<T> {
     }
 
     get(idx: number): T | undefined {
+        console.log(this.nums, idx, this.nums[idx]);
         return this.nums[idx];
     }
     removeAt(idx: number): T | undefined {
         const value = this.nums[idx];
         this.nums.splice(idx, 1);
+        this.length--;
         return value;
     }
 }
+
+const list = new ArrayList(5);
+list.append(5);
+list.append(7);
+list.append(9);
+
+// expect(list.get(2)).toEqual(9);
+// expect(list.removeAt(1)).toEqual(7);
+// expect(list.length).toEqual(2);
+
+list.append(11);
+// expect(list.removeAt(1)).toEqual(9);
+// expect(list.remove(9)).toEqual(undefined);
+// expect(list.removeAt(0)).toEqual(5);
+// expect(list.removeAt(0)).toEqual(11);
+// expect(list.length).toEqual(0);
+
+list.prepend(5);
+list.prepend(7);
+list.prepend(9);
+
+// expect(list.get(2)).toEqual(5);
+// expect(list.get(0)).toEqual(9);
+// expect(list.remove(9)).toEqual(9);
+// expect(list.length).toEqual(2);
+// expect(list.get(0)).toEqual(7);
+console.log(list);
