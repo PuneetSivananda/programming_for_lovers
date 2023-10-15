a802ad71a6b1d5c1e536e12fa196bb3aef26d0f3
PuneetSivananda <puneet.sivananda@gmail.com>
2023-10-15 18:51:17 +0530

    feat: maze solver

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/MazeSolver.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/MazeSolver.ts
@@ -1,3 +1,18 @@
+function walk(maze: string[], wall: string, curr: Point, end: Point): boolean {
+    // 1 Base case
+    // off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+    // 2 On a wall
+    
+}
+
 export default function solve(
     maze: string[],
     wall: string,
