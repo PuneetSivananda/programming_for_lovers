604668e92560bbea20641b7f0203255d02603b62
PuneetSivananda <puneet.sivananda@gmail.com>
2023-11-13 16:44:15 +0530

    feat: prepend and insertAt

--- primegeon/last_algorithms_class/kata-machine-master/src/day1/DoublyLinkedList.ts
+++ primegeon/last_algorithms_class/kata-machine-master/src/day1/DoublyLinkedList.ts
@@ -1,27 +1,63 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+};
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     prepend(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx > this.length) {
+            throw new Error("Oh oh");
+        }
 
-}
-    append(item: T): void {
+        if (idx === this.length) {
+            this.append(item);
+            return;
+        } else if (idx === 0) {
+            this.prepend(item);
+            return;
+        }
+        this.length++;
 
-}
-    remove(item: T): T | undefined {
+        let curr = this.head;
+        for (let i = 0; curr && i < idx; i++) {
+            curr = curr.next;
+        }
+        curr = curr as Node<T>;
+        const node = { value: item } as Node<T>;
+        node.next = curr;
+        node.prev = curr.prev;
 
-}
-    get(idx: number): T | undefined {
+        curr.prev = node;
+        if (curr.prev) {
+            curr.prev.next = curr;
+        }
+    }
 
-}
-    removeAt(idx: number): T | undefined {
+    append(item: T): void {}
 
-}
+    remove(item: T): T | undefined {}
+
+    get(idx: number): T | undefined {}
+
+    removeAt(idx: number): T | undefined {}
 }
