fd6ce9cd93229e7c24aa674f45b8356e903296e5
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-07 11:24:55 +0530

    feat: async func updates

--- fcc/interviews/walmart_interview1.js
+++ fcc/interviews/walmart_interview1.js
@@ -1,6 +1,13 @@
-// function sum(args){
-//     return (args)=>{return ...args}
-// }
+function sum(x) {
+  return (y) => {
+    return x + y;
+  };
+}
+
+const SumAll = (x) => (y) => y ? SumAll(x + y) : x;
+
+// console.log(sum(3)(3));
+// console.log(SumAll(3)(3)(4)());
 
 // console.log(sum(2)(3))
 // console.log(1 + '1' - 1);
@@ -19,38 +26,41 @@
 
 // console.log(typeof sayHi());
 
+async function PromiseFunc() {
+  return new Promise((resolve, reject) => {
+    resolve("sample");
+  });
+}
 
-// function PromiseFunc(){
-//     return new Promise((resolve, reject)=> {
-//         resolve("sample")    
-//     })
-// }
+async function getValue() {
+  let value = await PromiseFunc();
+  console.log(value);
+  return value;
+}
 
-// let value = PromiseFunc().then(val=>console.log(val))
+console.log(getValue());
 
-// console.log(value)
 // “()”
 // "(]"
 // “()[]{}”
 // “([])”
-function checkBrackets(inputA){
-    for(let a; a<inputA.length; a++){
-        for(let b; b<inputA.length; b++){
-            console.log(inputA[a])
-        }
+function checkBrackets(inputA) {
+  for (let a; a < inputA.length; a++) {
+    for (let b; b < inputA.length; b++) {
+      console.log(inputA[a]);
     }
+  }
 }
 
 // arr.map(item)=>{}
 
-function ownMap(inputArr, condition){
-    let outputArr = []
-    for(let i=0;i< inputArr.length;i++){
-        outputArr.push(condition(inputArr[i]))
-    }
-    return outputArr
+function ownMap(inputArr, condition) {
+  let outputArr = [];
+  for (let i = 0; i < inputArr.length; i++) {
+    outputArr.push(condition(inputArr[i]));
+  }
+  return outputArr;
 }
 
-console.log(ownMap([1,3,4], (i)=> i*2))
-[1,3,4].map(item=> item*2)
-
+// console.log(ownMap([1,3,4], (i)=> i*2))
+// [1,3,4].map(item=> item*2)
