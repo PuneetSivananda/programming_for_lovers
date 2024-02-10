2bb8eeb8c8c54db3c0ff6a71af08e45e966a0207
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-11 00:12:21 +0530

    feat: updated code

--- fcc/algorithms/23.NC-twoSum/main.go
+++ fcc/algorithms/23.NC-twoSum/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
+func twoSum(nums []int, target int) []int {
+	dict := make(map[int]int)
+	for idx, k := range nums {
+		if val, found := dict[target-k]; found {
+			return []int{val, idx}
+		}
+		dict[k] = idx
+	}
+	return nil
+}
 
+func main() {
+	// for a given list and the target value
+	// we must find the index of the numbers that will add up to the sum
+	ans1 := twoSum([]int{2, 7, 11, 15}, 9)
+	fmt.Println(ans1)
 }
