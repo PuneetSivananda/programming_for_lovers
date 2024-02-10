da191c7581de7180d681ea65e4e7f3fe4f225dda
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-11 00:51:17 +0530

    feat: a Very big sum code added

--- fcc/algorithms/24.aVerybigSum/main.go
+++ fcc/algorithms/24.aVerybigSum/main.go
@@ -4,44 +4,51 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
-	"strconv"
 	"strings"
 )
 
 func aVeryBigSum(ar []int64) int64 {
-	return 0
+	var sum, carry int64
+	for _, num := range ar {
+		sum += num
+		carry = sum / 10000000000
+		sum %= 10000000000
+	}
+	sum += carry
+
+	return sum
 }
 
 // Boilerplate for aVertBigSum
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	// reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
 
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
+	// stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	// checkError(err)
 
-	defer stdout.Close()
+	// defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+	// writer := bufio.NewWriterSize(stdout, 16*1024*1024)
 
-	arCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	checkError(err)
+	// arCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	// checkError(err)
 
-	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+	// arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
 
-	var ar []int64
+	// var ar []int64
 
-	for i := 0; i < int(arCount); i++ {
-		arItem, err := strconv.ParseInt(arTemp[i], 10, 64)
-		checkError(err)
-		ar = append(ar, arItem)
-	}
+	// for i := 0; i < int(arCount); i++ {
+	// 	arItem, err := strconv.ParseInt(arTemp[i], 10, 64)
+	// 	checkError(err)
+	// 	ar = append(ar, arItem)
+	// }
 
-	result := aVeryBigSum(ar)
+	result := aVeryBigSum([]int64{1000000001, 1000000002, 1000000003, 1000000004, 1000000005})
+	fmt.Println(result)
 
-	fmt.Fprintf(writer, "%d\n", result)
+	// fmt.Fprintf(writer, "%d\n", result)
 
-	writer.Flush()
+	// writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
