cb8cf905cbc70f6a969d12b7261846b555efac19
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-10 11:04:11 +0530

    feat: testting balanced branckets

--- fcc/interviews/walmart_interview2-balanced-bracket.js
+++ fcc/interviews/walmart_interview2-balanced-bracket.js
@@ -4,3 +4,36 @@
 // “([])”
 
 // use a stack to implement this
+
+function balancedBrackets(characters) {
+  const stack = [];
+  for (let i = 0; i <= characters.length; i++) {
+    if (characters[i] == "(" || characters[i] == "{" || characters[i] == "[") {
+      stack.push(characters[i]);
+      continue;
+    }
+
+    let check = stack.pop();
+    switch (check) {
+      case "(":
+        if (characters[i] == "]" || characters[i] == "}") {
+          return false;
+        }
+        break;
+      case "[":
+        if (characters[i] == ")" || characters[i] == "}") {
+          return false;
+        }
+        break;
+      case "{":
+        if (characters[i] == "]" || characters[i] == ")") {
+          return false;
+        }
+        break;
+    }
+  }
+  return stack.length == 0;
+}
+
+console.log(balancedBrackets("[{()}]") ? "Balanced" : "Not Balanced");
+console.log(balancedBrackets("[]{}()") ? "Balanced" : "Not Balanced");
