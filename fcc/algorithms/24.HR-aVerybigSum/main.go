4152493f712ad10ed1b2137ae1eb0331b93645c1
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-11 16:22:23 +0530

    feat: read and write to file

--- fcc/algorithms/24.HR-aVerybigSum/main.go
+++ fcc/algorithms/24.HR-aVerybigSum/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,35 +19,35 @@ func aVeryBigSum(ar []int64) int64 {
 
 // Boilerplate for aVertBigSum
 func main() {
-	// reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
 
-	// stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	// checkError(err)
+	stdout, err := os.Create("./output.txt")
+	checkError(err)
 
-	// defer stdout.Close()
+	defer stdout.Close()
 
-	// writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
 
-	// arCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
-	// checkError(err)
+	arCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
 
-	// arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
 
-	// var ar []int64
+	var ar []int64
 
-	// for i := 0; i < int(arCount); i++ {
-	// 	arItem, err := strconv.ParseInt(arTemp[i], 10, 64)
-	// 	checkError(err)
-	// 	ar = append(ar, arItem)
-	// }
+	for i := 0; i < int(arCount); i++ {
+		arItem, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		ar = append(ar, arItem)
+	}
 
-	// result := aVeryBigSum([]int64{1000000001, 1000000002, 1000000003, 1000000004, 1000000005})
-	result := aVeryBigSum([]int64{1001458909, 1004570889, 1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249})
-	fmt.Println(result)
+	result := aVeryBigSum(ar)
+	// result := aVeryBigSum([]int64{1001458909, 1004570889, 1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249})
+	// fmt.Println(result)
 
-	// fmt.Fprintf(writer, "%d\n", result)
+	fmt.Fprintf(writer, "%d\n", result)
 
-	// writer.Flush()
+	writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
