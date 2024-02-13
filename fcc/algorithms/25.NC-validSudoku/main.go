ec6a42f007b7c0feaeccfbba969fd94483609902
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-13 16:50:39 +0530

    feat: input file

--- fcc/algorithms/25.NC-validSudoku/main.go
+++ fcc/algorithms/25.NC-validSudoku/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+// usings sets check if each, row and column are having unique values
+// for every 3 by 3 grid check if any are duplicates
+// 	check the index of the 9 items first divide each index by 3 to get remainder
+//	eg: (2,3) => (2/3 = 0, 3/3 =1) its in index 0,1 for the final 3 by 3 matrix
 func main() {
 	fmt.Println("hello")
 }
