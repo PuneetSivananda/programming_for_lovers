33dd780aad3e47f7fb36e5814035198f5ce97588
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-14 15:42:09 +0530

    feat: how to get inputs

--- fcc/algorithms/interview2-SA-sigTuple/main.go
+++ fcc/algorithms/interview2-SA-sigTuple/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Sample")
+	player1 := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("Enter Prompt: ")
+		player1Text, _ := player1.ReadString('\n')
+		player1Text = strings.Replace(player1Text, "\n", "", -1)
+		fmt.Println(player1Text)
+		if player1Text == "exit" {
+			break
+		}
+	}
+
 }
