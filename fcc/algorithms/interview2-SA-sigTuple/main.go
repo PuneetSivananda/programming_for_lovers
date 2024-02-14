65a33529675d50750121fd754cebaa04b16b7b26
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-14 15:55:20 +0530

    chore: added printing board

--- fcc/algorithms/interview2-SA-sigTuple/main.go
+++ fcc/algorithms/interview2-SA-sigTuple/main.go
@@ -7,6 +7,16 @@ import (
 	"strings"
 )
 
+func print_board(board [9]string) {
+	row1 := fmt.Sprintf("| %s | %s | %s |\n", board[0], board[1], board[2])
+	row2 := fmt.Sprintf("| %s | %s | %s |\n", board[3], board[4], board[5])
+	row3 := fmt.Sprintf("| %s | %s | %s |\n", board[6], board[7], board[8])
+	fmt.Printf(row1)
+	fmt.Printf(row2)
+	fmt.Printf(row3)
+	fmt.Println()
+}
+
 func main() {
 	// initialize board
 	// board = [" " for x in range(9)]
@@ -14,6 +24,8 @@ func main() {
 	for i := 0; i < 9; i++ {
 		board[i] = "0"
 	}
+
+	print_board(board)
 
 	player1 := bufio.NewReader(os.Stdin)
 	for {
