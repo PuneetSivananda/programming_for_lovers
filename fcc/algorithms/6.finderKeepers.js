18671ec5aa884ddc8471b9a851a4cd856767f308
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-16 18:57:31 +0530

    feat: createing using array prototypes

--- fcc/algorithms/6.finderKeepers.js
+++ fcc/algorithms/6.finderKeepers.js
@@ -1,3 +1,13 @@
+Array.prototype.findElementmap = function (callback) {
+  const result = [];
+  for (let ix = 0; ix < this.length; ix++) {
+    if (callback(this[ix]) != false) {
+      result.push(this[ix]);
+    }
+  }
+  return result;
+};
+
 function findElement(arr, func) {
   let num = 0;
   for (let i = 0; i < arr.length; i++) {
@@ -11,5 +21,12 @@ console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
 console.log(
   findElement([1, 3, 5, 8, 9, 10], function (num) {
     return num % 2 === 0;
+  })
+);
+
+// using prototyping
+console.log(
+  [1, 3, 5, 8, 9, 10].findElementmap((item) => {
+    return item % 2 === 0;
   })
 );
