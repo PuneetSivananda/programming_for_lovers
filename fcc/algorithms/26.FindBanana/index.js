516f5efcf16e36e02b833c3b8228e448b40e10f7
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-16 17:42:46 +0530

    feat: updated code

--- fcc/algorithms/26.FindBanana/index.js
+++ fcc/algorithms/26.FindBanana/index.js
@@ -2,3 +2,14 @@
 // Input String: "bawnartenyabtyanuiaurnwaananab"
 // Output: banana banana banana
 
+function findBananaStrings(inputString) {
+  let unique = Array.from(new Set("banana"));
+  inputString.split("").forEach((letter) => {
+    if (unique.includes(letter)) {
+      console.log(`${letter}: banana`);
+    }
+  });
+  return;
+}
+
+findBananaStrings("bawnartenyabtyanuiaurnwaananab");
