bbb5ff8bdf6f301a7fd2a32fc91e39697560f07f
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-20 15:19:23 +0530

    feat: refactor the entire code as a single next app

---
+++ fcc/react-projects/src/app/syntax-tailwind-recreate/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="text-600">Syntax Page Screen</div>
+    </main>
+  );
+}
