1e3a4820cb7b9c836dadeed892526d9ec6f98d79
PuneetSivananda <puneet.sivananda@gmail.com>
2024-02-24 21:02:44 +0530

    feat: addded the banner component

--- fcc/react-projects/src/app/syntax-tailwind-recreate/src/pages/Header.tsx
+++ fcc/react-projects/src/app/syntax-tailwind-recreate/src/pages/Header.tsx
@@ -21,15 +21,15 @@ export default function Header() {
             position: "fixed",
             top: "1px",
             left: "1px",
-            //     width: 1px;
-            // height: 0;
-            // padding: 0;
-            // margin: -1px;
-            // overflow: hidden;
-            // clip: rect(0, 0, 0, 0);
-            // white-space: nowrap;
-            // border-width: 0;
-            // display: none;
+            width: "1px",
+            height: "0",
+            padding: "0",
+            margin: "-1px",
+            overflow: "hidden",
+            clip: "rect(0, 0, 0, 0)",
+            whiteSpace: "nowrap",
+            borderWidth: 0,
+            display: "none",
           }}
         ></div>
       </div>
