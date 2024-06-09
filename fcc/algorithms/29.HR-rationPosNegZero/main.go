62ce4da487195782fda2446b9e877003731f4025
PuneetSivananda <puneet.sivananda@gmail.com>
2024-06-09 22:41:46 +0530

    feat: completed count of types

--- fcc/algorithms/29.HR-rationPosNegZero/main.go
+++ fcc/algorithms/29.HR-rationPosNegZero/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var arr []int32 = []int32{5, 6, 7, 6, 0, -4, -6}
+	var positiveCount, negativeCount, zeroCount int
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > 0 {
+			positiveCount++
+		} else if arr[i] < 0 {
+			negativeCount++
+		} else if arr[i] == 0 {
+			zeroCount++
+		}
+	}
+	fmt.Println(positiveCount, negativeCount, zeroCount)
 
 }
