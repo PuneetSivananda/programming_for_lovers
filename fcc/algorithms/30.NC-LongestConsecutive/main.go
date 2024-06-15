8f2aba2e6629d804ffc0c93bf99c1be4f3fdc681
PuneetSivananda <puneet.sivananda@gmail.com>
2024-06-15 11:24:07 +0530

    feat: added code

--- fcc/algorithms/30.NC-LongestConsecutive/main.go
+++ fcc/algorithms/30.NC-LongestConsecutive/main.go
@@ -1,8 +1,19 @@
 package main
 
+import "fmt"
+
 // Input: nums = [2,20,4,10,3,4,5]
 // Output: 4
 
-func main(){
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
 
+	var arr = make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &arr[i])
+	}
+
+	fmt.Printf("%v", arr)
 }
