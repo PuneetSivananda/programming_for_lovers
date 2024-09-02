0399906db519d701013ee38c806d83b15ba8a8cd
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-02 14:44:56 +0530

    feat: interview code

---
+++ fcc/interviews/interview_10.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+// it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// func isPalindrome(s string) bool {}
+// _______
+// _______
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// ToLower
+// Contains
+// Reverse using for loop
+// check strings
+
+func isPalindrome(s string) bool {
+	asciChars := "abcdefghijklmnopqrstuvxyz0123456789"
+	lowerS := strings.ToLower(s)
+	var onlyAsciChars []string
+	for i := 0; i <= len(lowerS)-1; i++ {
+		if strings.Contains(asciChars, string(lowerS[i])) {
+			onlyAsciChars = append(onlyAsciChars, string(lowerS[i]))
+		}
+	}
+	lenS := len(onlyAsciChars)
+	var reversedChars []string
+	for i := lenS; i > 0; i-- {
+		reversedChars = append(reversedChars, onlyAsciChars[i])
+
+	}
+
+	if reversedChars == onlyAsciChars {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func main() {
+	isPalindrome("sample")
+	fmt.Printf("Hello, World!")
+}
