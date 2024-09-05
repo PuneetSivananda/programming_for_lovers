a12b66cc6d9a464a1b60f7e095e5327634c604a5
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-05 17:21:56 +0530

    feat: reading data from api

---
+++ fcc/interviews/interview_11.py
@@ -0,0 +1,28 @@
+"""
+# import regex
+# sampleList = ['abcxyz', 'abcuvw','mnoxyz']
+
+# expr = '^(abc)*(xyz)$'
+# testexpr = regex(expr)
+# print(testexpr.test('abc123xyz'))
+"""
+inputList = [7, 8, 4, 3]
+def swap(a,b):
+    return b, a
+
+def bubbleSort(inputList):
+    for item in range(inputList):
+        for item in range(inputList):
+            if(inputList[i]>inputList[j]):
+                swap(inputList[i], inputList(j))  
+    return inputList
+
+print(bubbleSort(inputList))
+
+sorted(inputList)
+
+def ownFunction(a, b, ...rest):
+    len(args) ==2:
+        continue
+    break "need proper argumesnts"
+
---
+++ fcc/interviews/interview_12.go
@@ -0,0 +1,45 @@
+// Concurrency in go
+// https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=10
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func fetchCatsApi() interface{} {
+	type data struct {
+		Id     string `json:"id"`
+		Url    string `json:"url"`
+		Width  int    `json:"width"`
+		Height int    `json:"heigth"`
+	}
+
+	count := "10"
+	url := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=%s", count)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching data")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Unable to read response body")
+	}
+
+	var images []data
+	err = json.Unmarshal(body, &images)
+	if err != nil {
+		fmt.Printf("Error Decoding Data")
+
+	}
+	return images
+}
+
+func main() {
+	cats := fetchCatsApi()
+	fmt.Println(cats)
+}
