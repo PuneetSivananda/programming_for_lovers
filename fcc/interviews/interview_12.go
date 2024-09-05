27e3ea2d7784a87a817bab2016d8268d97ba8df1
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-05 21:22:44 +0530

    feat: this does the fetching one by one

--- fcc/interviews/interview_12.go
+++ fcc/interviews/interview_12.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
+	"time"
 )
 
 type data struct {
@@ -17,8 +19,7 @@ type data struct {
 }
 
 func fetchCatsApiWithoutChanels() []data {
-
-	count := "10"
+	count := "1"
 	url := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=%s", count)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -40,10 +41,36 @@ func fetchCatsApiWithoutChanels() []data {
 	return images
 }
 
-func main() {
-	cats := fetchCatsApiWithoutChanels()
-	for _, cat := range cats {
-		fmt.Println(cat.Url)
+func fetchCatsApiWithChannels(ch chan<- string, wg *sync.WaitGroup) []data {
+	defer wg.Done()
+	count := "1"
+	url := fmt.Sprintf("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=%s", count)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching data")
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Unable to read response body")
 	}
 
+	var images []data
+	err = json.Unmarshal(body, &images)
+	if err != nil {
+		fmt.Printf("Error Decoding Data")
+
+	}
+	// ch <- fmt.Sprintf("This is the data %s", data)
+	return images
+}
+
+func main() {
+	startNow := time.Now()
+	for i := 0; i < 10; i++ {
+		cat := fetchCatsApiWithoutChanels()
+		fmt.Println(cat[0].Url)
+	}
+	fmt.Println("Elapased Time Since: ", time.Since(startNow))
 }
