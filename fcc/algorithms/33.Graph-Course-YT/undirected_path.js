4336cc1f0b84bfe74c8872af117c628c52fc500c
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-09 23:29:35 +0530

    wip: adjacency list

---
+++ fcc/algorithms/33.Graph-Course-YT/undirected_path.js
@@ -0,0 +1,32 @@
+function buildGraph(edges) {
+  const graph = {};
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) {
+      graph[a] = [];
+    }
+    if (!(b in graph)) {
+      graph[b] = [];
+    }
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+}
+
+function undirected_path(edges, start, dest) {
+  // Convert to adjacency list
+  //   do dfs or bfs
+  const graph = buildGraph(edges);
+  console.log(graph);
+}
+
+const edges = [
+  ["i", "j"],
+  ["k", "i"],
+  ["m", "k"],
+  ["k", "l"],
+  ["o", "n"],
+];
+
+undirected_path(edges, "j", "m");
