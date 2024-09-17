ad5a78828512e848e68224f3a7d23f054f6e7ef3
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-17 11:58:32 +0530

    feat: solved has_path using recursion

--- fcc/algorithms/33.Graph-Course-YT/has_path.go
+++ fcc/algorithms/33.Graph-Course-YT/has_path.go
@@ -17,11 +17,25 @@ var graph = `{
 
 var graphData map[string][]string
 
+func has_path(graph map[string][]string, src string, dest string) bool {
+	if src == dest {
+		return true
+	}
+	for _, neigh := range graph[src] {
+		if has_path(graph, neigh, dest) {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	var err = json.Unmarshal([]byte(graph), &graphData)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+
+	fmt.Println(has_path(graphData, "f", "e"))
 
 	for k, v := range graphData {
 		fmt.Println(k, "=", v)
