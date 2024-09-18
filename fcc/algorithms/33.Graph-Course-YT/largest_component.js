64e53eca90f66171f3424536f89402fa113fd268
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-18 18:20:08 +0530

    feat: largest size

--- fcc/algorithms/33.Graph-Course-YT/largest_component.js
+++ fcc/algorithms/33.Graph-Course-YT/largest_component.js
@@ -1,19 +1,34 @@
 const graph = {
-  3: [],
-  4: ["6"],
-  6: ["4", "5", "7", "8"],
-  8: ["6"],
-  7: ["6"],
-  5: ["6"],
-  1: ["2"],
-  2: ["1"],
+  0: ["8", "1", "5"],
+  1: ["0"],
+  5: ["0", "8"],
+  8: ["0", "5"],
+  2: ["3", "4"],
+  3: ["2", "4"],
+  4: ["3", "2"],
 };
 
-const explore = (graph, current, visited) => {
-  if (visited.has(current)) return false;
-  visited.add(current);
-  for (let neigh of graph[current]) {
-    explore(graph, neigh, visited);
+const exploreSize = (graph, node, visited) => {
+  if (visited.has(node)) return 0;
+  visited.add(node);
+  let size = 1;
+  for (let neigh of graph[node]) {
+    size += exploreSize(graph, neigh, visited);
   }
-  return true;
+  return size;
 };
+
+const largestComponent = (graph) => {
+  const visited = new Set();
+  let largestVisited = 0;
+
+  for (let node in graph) {
+    let size = exploreSize(graph, node, visited);
+    if (size > largestVisited) {
+      largestVisited = size;
+    }
+  }
+  return largestVisited;
+};
+
+console.log(largestComponent(graph));
