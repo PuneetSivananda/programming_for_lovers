94fd457d303f89c74b3fc575787f1a287a767085
PuneetSivananda <puneet.sivananda@gmail.com>
2024-09-18 19:19:26 +0530

    feat: build graph

--- fcc/algorithms/33.Graph-Course-YT/shortest_path.js
+++ fcc/algorithms/33.Graph-Course-YT/shortest_path.js
@@ -1,8 +1,35 @@
 const edges = [
-    ["w", "x"],
-    ["x", "y"],
-    ["z", "y"],
-    ["z", "v"],
-    ["w", "v"],
-    
-]
+  ["w", "x"],
+  ["x", "y"],
+  ["z", "y"],
+  ["z", "v"],
+  ["w", "v"],
+];
+
+function buildGraph(edges) {
+  let graph = {};
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (graph[a] == undefined) {
+      graph[a] = [];
+    }
+    if (graph[b] == undefined) {
+      graph[b] = [];
+    }
+    // if (!(a in graph)) {
+    //   graph[a] = [];
+    // }
+    // if (!(b in graph)) {
+    //   graph[b] = [];
+    // }
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+}
+
+function shortest_path() {
+  const graph = buildGraph(edges);
+  console.log(graph);
+}
+console.log(shortest_path());
